ebeb6a024bea941f601157abfe7375f22dca7fd3
Hoa <h_cong@hetic.eu>
2025-12-03 14:47:23 +0100

    feature: enhance cesar cipher function to support encryption mode

--- correction.py
+++ correction.py
@@ -1,10 +1,24 @@
-def cesar_cipher(text, key):
+def cesar_cipher(text, key, cipher = True):
+
     crypted_text = ""
+    
+    key = key if cipher else -key #terary
 
     for char in text:
-        chrypted_char = chr((ord(char) + key % 26))
-        crypted_text += chrypted_char
+        crypted_char = chr((ord(char) + key) % 1_114_112)
+        crypted_text += crypted_char
 
     return crypted_text
 
-print(cesar_cipher("hello", 3))
+
+def brute_force_cesar_cipher(text):
+    for key in range(1, 1_114_112):
+        potential_plaintext = cesar_cipher(text, key, cipher=False)
+        print(key)
+        print(potential_plaintext)
+        print("-------------------")
+
+        
+    return
+
+print(brute_force_cesar_cipher('michel'))
